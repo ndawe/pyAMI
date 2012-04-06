35ba907eb5623fa6d4eac26af28086a691ce5033
/O=GRID-FR/C=FR/O=CNRS/OU=LPSC/CN=Fabian Lambert </O=GRID-FR/C=FR/O=CNRS/OU=LPSC/CN=Fabian Lambert@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-04-06 08:02:35 +0000

    refs #2656 Fixing bug in custom AMIHTTPConnection and AMIHTTPSConnection
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@3625 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- pyAMI/httpconnection.py
+++ pyAMI/httpconnection.py
@@ -21,27 +21,34 @@ class AMIHTTPConnection(HTTPConnection):
 
         if not (self.proxy_host and self.proxy_port):
             HTTPConnection.connect(self)
-
-        msg = "getaddrinfo returns an empty list"
-        for res in socket.getaddrinfo(self.proxy_host, self.proxy_port, 0,
-                                    socket.SOCK_STREAM):
-            af, socktype, proto, canonname, sa = res
-            try:
-                self.sock = socket.socket(af, socktype, proto)
-                if self.debuglevel > 0:
-                    print "connect: (%s, %s)" % (self.proxy_host, self.proxy_port)
-                self.sock.connect(sa)
-            except socket.error, msg:
-                if self.debuglevel > 0:
-                    print 'connect fail:', (self.proxy_host, self.proxy_port)
-                if self.sock:
-                    self.sock.close()
-                self.sock = None
-                continue
-            break
-        if not self.sock:
-            raise socket.error, msg
-
+        else:
+            msg = "getaddrinfo returns an empty list"
+            for res in socket.getaddrinfo(self.proxy_host, self.proxy_port, 0,
+                                        socket.SOCK_STREAM):
+                af, socktype, proto, canonname, sa = res
+                try:
+                    self.sock = socket.socket(af, socktype, proto)
+                    if self.debuglevel > 0:
+                        print "connect: (%s, %s)" % (self.proxy_host, self.proxy_port)
+                    self.sock.connect(sa)
+                except socket.error, msg:
+                    if self.debuglevel > 0:
+                        print 'connect fail:', (self.proxy_host, self.proxy_port)
+                    if self.sock:
+                        self.sock.close()
+                    self.sock = None
+                    continue
+                break
+            if not self.sock:
+                raise socket.error, msg
+        
+    def putrequest(self, method, url, skip_host=0, skip_accept_encoding=0):
+        """Recover the full URL path together with host"""
+        full_url = url
+        urlPref = "http://"
+        if not url.startswith(urlPref):
+            full_url = '%s%s:%s%s' % (urlPref, self.host, self.port, url)
+        HTTPConnection.putrequest(self, method, full_url, skip_host, skip_accept_encoding)
 
 class AMIHTTPSConnection(HTTPSConnection):
 
@@ -62,23 +69,31 @@ class AMIHTTPSConnection(HTTPSConnection):
 
         if not (self.proxy_host and self.proxy_port):
             HTTPSConnection.connect(self)
-
-        msg = "getaddrinfo returns an empty list"
-        for res in socket.getaddrinfo(self.proxy_host, self.proxy_port, 0,
-                                socket.SOCK_STREAM):
-            af, socktype, proto, canonname, sa = res
-            try:
-                self.sock = socket.socket(af, socktype, proto)
-                if self.debuglevel > 0:
-                    print "connect: (%s, %s)" % (self.proxy_host, self.proxy_port)
-                self.sock.connect(sa)
-            except socket.error, msg:
-                if self.debuglevel > 0:
-                    print 'connect fail:', (self.proxy_host, self.proxy_port)
-                if self.sock:
-                    self.sock.close()
-                self.sock = None
-                continue
-            break
-        if not self.sock:
-            raise socket.error, msg
+        else:
+            msg = "getaddrinfo returns an empty list"
+            for res in socket.getaddrinfo(self.proxy_host, self.proxy_port, 0,
+                                    socket.SOCK_STREAM):
+                af, socktype, proto, canonname, sa = res
+                try:
+                    self.sock = socket.socket(af, socktype, proto)
+                    if self.debuglevel > 0:
+                        print "connect: (%s, %s)" % (self.proxy_host, self.proxy_port)
+                    self.sock.connect(sa)
+                except socket.error, msg:
+                    if self.debuglevel > 0:
+                        print 'connect fail:', (self.proxy_host, self.proxy_port)
+                    if self.sock:
+                        self.sock.close()
+                    self.sock = None
+                    continue
+                break
+            if not self.sock:
+                raise socket.error, msg
+        
+    def putrequest(self, method, url, skip_host=0, skip_accept_encoding=0):
+        """Recover the full URL path together with host"""
+        full_url = url
+        urlPref = "https://"
+        if not url.startswith(urlPref):
+            full_url = '%s%s:%s%s' % (urlPref, self.host, self.port, url)
+        HTTPSConnection.putrequest(self, method, full_url, skip_host, skip_accept_encoding)
