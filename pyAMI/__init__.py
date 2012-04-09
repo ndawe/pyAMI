fb526d395be2b4669f64d413bd15fb887a8acea0
/C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44 </C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-04-09 06:37:49 +0000

    print the credits
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@3646 29852e1a-7735-49d4-a4ac-1f29da7335c7

---
+++ pyAMI/__init__.py
@@ -0,0 +1,17 @@
+"""
+                _    __  __ ___
+ _ __  _   _   / \  |  \/  |_ _|
+| '_ \| | | | / _ \ | |\/| || |
+| |_) | |_| |/ ___ \| |  | || |
+| .__/ \__, /_/   \_\_|  |_|___|
+|_|    |___/
+              %s
+Authors:
+
+Solveig Albrand
+Noel Dawe
+Jérôme Fulachier
+Fabian Lambert
+"""
+from pyAMI.info import VERSION
+__doc__ = __doc__ % VERSION
