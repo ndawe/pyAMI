3b9a80a099211b788b6bdc44fb77cf10172fefc2
/C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44 </C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-04-12 16:48:54 +0000

    increment version
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@3661 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- pyAMI/info.py
+++ pyAMI/info.py
@@ -15,7 +15,7 @@ Fabian Lambert
 
 """
 
-VERSION = '4.0.0a2'
+VERSION = '4.0.0a3'
 AUTHOR_EMAIL = 'atlas-bookkeeping@cern.ch'
 URL = 'https://end.web.cern.ch/end/projects/pyAMI/'
 DOWNLOAD_URL = 'https://end.web.cern.ch/end/downloads/pyAMI/pyAMI-%s.tar.gz' % VERSION
