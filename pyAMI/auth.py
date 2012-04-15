f05abeba65e68f33220ef8bb8ced305c16639b44
/C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44 </C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-04-15 22:46:30 +0000

    warn user about password encoding
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@3665 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- pyAMI/auth.py
+++ pyAMI/auth.py
@@ -15,6 +15,9 @@ if os.path.isfile(AMI_CONFIG):
 def create_auth_config():
 
     config = AMIConfig()
+    # warn user about encoded password
+    config.add_comment('AMI', 'Your password is only base64 encoded here and can be decoded.')
+    config.add_comment('AMI', 'Please do not share this file publicly.')
     config.set('AMI', 'AMIUser', raw_input('Username: '))
     config.set('AMI', 'AMIPass', base64.b64encode(getpass()))
     f = open(AMI_CONFIG, 'w')
--- pyAMI/config.py
+++ pyAMI/config.py
@@ -1,9 +1,8 @@
 """
-atlasmeta AMI configuration / initialisation.
+pyAMI configuration
 """
 
-from ConfigParser import *
-from os.path import exists, expandvars
+from ConfigParser import ConfigParser
 
 
 class AMIConfig(ConfigParser):
@@ -24,21 +23,47 @@ class AMIConfig(ConfigParser):
         self.set('AMI', 'AMIPass', '')
         self.set('AMIMISC', 'separator', ';')
 
-    def include(self, extraParamsDict):
+    def include(self, params):
         """
         This method only updates mandatory configuration parameters
-        and their values with those specified in extraParamsDict.
+        and their values with those specified in params.
         Returns a dictionary with mandatory parameters in 'AMI' section
-        and extraParamsDict merged (priority given to extraParamsDict parameters).
+        and params merged (priority given to params parameters).
         """
         _t = {}
         # Populate _t with current configuration from config file
         for _opt in self.options('AMI'):
             _t[ _opt ] = self.get('AMI', _opt)
         # Overwrite and/or add new parameters to _t
-        for x in extraParamsDict:
-            _t[ x ] = extraParamsDict[ x ]
+        for name, value in params.items():
+            _t[name] = value
             # find correct section to update option
-            if self.has_option('AMI', x):
-                self.set('AMI', x, _t[ x ])
+            if self.has_option('AMI', name):
+                self.set('AMI', name, value)
         return _t
+
+    def add_comment(self, section, comment):
+
+        self.set(section, '; %s' % (comment,), None)
+
+    def write(self, fp):
+        """
+        Write an .ini-format representation of the configuration state.
+        """
+        if self._defaults:
+            fp.write("[%s]\n" % ConfigParser.DEFAULTSECT)
+            for (key, value) in self._defaults.items():
+                self._write_item(fp, key, value)
+            fp.write("\n")
+        for section in self._sections:
+            fp.write("[%s]\n" % section)
+            for (key, value) in self._sections[section].items():
+                self._write_item(fp, key, value)
+            fp.write("\n")
+
+    def _write_item(self, fp, key, value):
+
+        if key.startswith(';') and value is None:
+            fp.write("%s\n" % (key,))
+        else:
+            fp.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
