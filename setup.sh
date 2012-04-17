003ce27cdafce6f9dc988047d5031872cb6a24fa
/C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44 </C=CA/O=Grid/OU=westgrid.ca/CN=Noel Dawe_44@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-04-17 04:35:13 +0000

    support for PYAMI_LIBRARY_PATH
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@3668 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 export PATH=/afs/cern.ch/atlas/software/tools/atlasmeta/bin:$PATH
-export PYTHONPATH=/afs/cern.ch/atlas/software/tools/atlasmeta/lib/python2.4/site-packages${PYTHONPATH:+:$PYTHONPATH}
+export PYAMI_LIBRARY_PATH=/afs/cern.ch/atlas/software/tools/atlasmeta/lib/python2.4/site-packages
