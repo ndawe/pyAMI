f59516334badd7ba675234612b260e6cc325ddf8
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-06-14 17:25:12 +0000

    make setup.sh work with both bash and zsh
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@4136 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+# This script will work in either bash or zsh.
+
 # deterine path to this script
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-SOURCE_PYAMI_SETUP="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE_PYAMI_SETUP" ] ; do SOURCE_PYAMI_SETUP="$(readlink "$SOURCE_PYAMI_SETUP")"; done
+SOURCE_PYAMI_SETUP="${BASH_SOURCE[0]:-$0}"
+
+DIR_PYAMI_SETUP="$( dirname "$SOURCE_PYAMI_SETUP" )"
+while [ -h "$SOURCE_PYAMI_SETUP" ]
+do 
+  SOURCE_PYAMI_SETUP="$(readlink "$SOURCE_PYAMI_SETUP")"
+  [[ $SOURCE_PYAMI_SETUP != /* ]] && SOURCE_PYAMI_SETUP="$DIR_PYAMI_SETUP/$SOURCE_PYAMI_SETUP"
+  DIR_PYAMI_SETUP="$( cd -P "$( dirname "$SOURCE_PYAMI_SETUP"  )" && pwd )"
+done
 DIR_PYAMI_SETUP="$( cd -P "$( dirname "$SOURCE_PYAMI_SETUP" )" && pwd )"
+
 echo "sourcing ${SOURCE_PYAMI_SETUP}..."
 
 export PATH=${DIR_PYAMI_SETUP}/bin${PATH:+:$PATH}
