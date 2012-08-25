d2b118d17a54d640948b140e30f2e74c9681a7c5
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-08-25 12:53:01 +0000

    fix 2012 default data12_8TeV
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@4686 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- pyAMI/defaults.py
+++ pyAMI/defaults.py
@@ -8,7 +8,10 @@ import datetime
 
 # delay by half a year
 YEAR = (datetime.date.today() - datetime.timedelta(182.5)).year % 1000
-PROJECT = 'data%d_7TeV' % YEAR
+if YEAR < 12:
+    PROJECT = 'data%d_7TeV' % YEAR
+else:
+    PROJECT = 'data%d_8TeV' % YEAR
 STREAM = 'physics_JetTauEtmiss'
 TYPE = 'AOD'
 PRODSTEP = 'merge'
