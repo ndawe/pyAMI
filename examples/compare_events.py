9624905fa4f09767e8f0b1b7e36c7c3d27ff670f
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-10-05 19:09:18 +0000

    add example for comparing number of events between AOD and NTUP_TAU
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@4830 29852e1a-7735-49d4-a4ac-1f29da7335c7

---
+++ examples/compare_events.py
@@ -0,0 +1,18 @@
+from pyAMI.client import AMIClient
+from pyAMI.query import get_datasets, get_provenance, get_dataset_info
+
+
+client = AMIClient()
+
+ntup_tau = get_datasets(client, 'p1130', parent_type='AOD', fields='events', flatten=True)
+
+for nevents, ds in ntup_tau:
+    print "checking %s ..." % ds
+    print "%s events in NTUP_TAU" % nevents
+    aod = get_provenance(client, ds, type='AOD').values()[0][0]
+    aod_events = get_dataset_info(client, aod).info['totalEvents']
+    print "checking %s ..." % aod
+    print "%s events in AOD" % aod_events
+    if int(aod_events) != int(nevents):
+        print "MISMATCH!"
+    print '-' * 20
