745cd149eb6f05db1ad09f7f3c2ab5b42e0adf72
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-10-23 06:17:29 +0000

    updates
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@4919 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- install_afs.sh
+++ install_afs.sh
@@ -3,7 +3,7 @@
 PYAMI_AFS_PATH=/afs/cern.ch/atlas/software/tools/atlasmeta
 if [ -d ${PYAMI_AFS_PATH} ]
 then
-    cp setup.sh $PYAMI_AFS_PATH
+    cp ./etc/setup.sh $PYAMI_AFS_PATH
     source $PYAMI_AFS_PATH/setup.sh
     python setup.py install --afs-install --prefix=$PYAMI_AFS_PATH
 else
