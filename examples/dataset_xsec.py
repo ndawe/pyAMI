11c45fb4921ab45a0ba8476099a1b06b0b3b1245
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2012-12-17 07:56:30 +0000

    simplify examples
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@5065 29852e1a-7735-49d4-a4ac-1f29da7335c7

--- examples/dataset_xsec.py
+++ examples/dataset_xsec.py
@@ -1,12 +1,7 @@
 from pyAMI.client import AMIClient
 from pyAMI.query import get_dataset_xsec_effic
-from pyAMI.auth import AMI_CONFIG, create_auth_config
-import os
 
 client = AMIClient()
-if not os.path.exists(AMI_CONFIG):
-    create_auth_config()
-client.read_config(AMI_CONFIG)
 
 dataset = 'mc11_7TeV.125206.PowHegPythia_VBFH130_tautauhh.merge.NTUP_TAUMEDIUM.e893_s1310_s1300_r2730_r2780_p795'
 xsec, effic = get_dataset_xsec_effic(client, dataset)
--- examples/list_runs.py
+++ examples/list_runs.py
@@ -1,12 +1,7 @@
 from pyAMI.client import AMIClient
 from pyAMI.query import get_runs, get_periods_for_run
-from pyAMI.auth import AMI_CONFIG, create_auth_config
-import os
 
 client = AMIClient()
-if not os.path.exists(AMI_CONFIG):
-    create_auth_config()
-client.read_config(AMI_CONFIG)
 
 runs = get_runs(client, periods=['B', 'K2'], year=11)
 print runs
