ce4e3980ec66df9c5d3845e6f5d31e2e057ce2d7
/C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382 </C=CA/O=Grid/OU=westgrid.ca/CN=Edmund Dawe ptu-382@29852e1a-7735-49d4-a4ac-1f29da7335c7>
2013-02-22 19:28:36 +0000

    add setup_pyAMI with deprecationwarning
    git-svn-id: https://lpsc.in2p3.fr/svn/AMI/trunk/AMIWebServiceClient/pyAMI@5253 29852e1a-7735-49d4-a4ac-1f29da7335c7

---
+++ etc/setup_pyAMI.py
@@ -0,0 +1,5 @@
+# this file only exists for backward compatibility
+import warnings
+
+warnings.simplefilter('default')
+warnings.warn("import setup_pyAMI is no longer required", DeprecationWarning)
