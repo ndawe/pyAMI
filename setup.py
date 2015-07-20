257dcb708409e12afe9a878c5039539934db7dd8
Noel Dawe <noel.dawe@gmail.com>
2015-07-20 13:14:54 +0200

    don't use distribute

--- setup.py
+++ setup.py
@@ -47,8 +47,6 @@ if use_lxml and os.getenv('PYAMI_NO_LXML') not in ('1', 'true'):
 
 kw = {}
 if use_distribute and os.getenv('PYAMI_NO_DISTRIBUTE') not in ('1', 'true'):
-    from distribute_setup import use_setuptools
-    use_setuptools()
     from setuptools import setup
     kw['install_requires'] = requires
     kw['entry_points'] = {
